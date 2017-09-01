e4c7c485b1bcf48fd3e66afdee1150a31010e2b5
Casey Wilson <cawilson1@g.cofc.edu>
2017-09-01 14:10:03 -0400

    initial commit

---
+++ graphIsoProtoApp/src/main/java/graphIsoProto/gui/GraphIsoProtoUIController.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package graphIsoProto.gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author caseyubvm
+ */
+public class GraphIsoProtoUIController{// implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    //@Override
+    //public void initialize(URL url, ResourceBundle rb) {
+      //  System.out.println("test");
+    //}   
+    
+    public void buttonClicked(){
+        System.out.println("buttonClicked");
+    }
+    
+}
